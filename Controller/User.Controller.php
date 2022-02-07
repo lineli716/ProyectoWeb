1d99fee76f70e39b67c725cadacd757708fe86e0
lineli716 <201800138@colsanjose.edu.gt>
2022-02-07 13:45:19 -0600

    cambio
    cambio

--- Controller/User.Controller.php
+++ Controller/User.Controller.php
@@ -1,15 +1,50 @@
 <?php
     class User
-    
     {
-        public function Inicio()
+        public $smarty;
+        public $usuario;
+        public function __construct()
         {
-            echo "en usuario";
+            $this->usuario=new Usuario();
+            $this->smarty=new Smarty();
+            session_start();
         }
 
-        public function Fin()
+        public function BuscarUsuario()
         {
-            echo "en usuario fin";
-        }
+            $user=$_POST['user'];
+            $pass=$_POST['pass'];
+            
+            $u=$this->usuario->BuscarUsuario($user,$pass);
+
+            if($u->num_rows==1)
+            {
+                $arr=array();
+                while ($fila=mysqli_fetch_assoc($u))
+                {
+                    array_push($arr,$fila);
+                }
+                $_SESSION['id_Usuario']=$arr[0]['idUsuario'];
+                if ($arr[0]['Rol_idRol']==1)
+                {
+                    $this->smarty->assign('title','Administrador');
+                    $this->smarty->display('Administrador.tpl');
+                }
+
+            } 
+
+            else if($u->num_rows>1)
+            {
+                $this->smarty->assign('title', 'Home');
+                $this->smarty->display('Home.tpl');
+            }
+            else
+            {
+                $this->smarty->assign('title','Home');
+                $this->smarty->display('Home.tpl');
+            }  
+
+        } 
+
     }
 ?>
