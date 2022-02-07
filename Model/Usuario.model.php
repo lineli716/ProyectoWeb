a37414c7dd60d1e94a5c5892cd2bd24c7efcd9f5
lineli716 <201800138@colsanjose.edu.gt>
2022-02-07 13:47:33 -0600

    cambio
    cambio

--- Model/Usuario.model.php
+++ Model/Usuario.model.php
@@ -2,7 +2,7 @@
     class Usuario
     {
         public $con;
-        public function_ construct()
+        public function_construct()
         {
             $this->con=new Conexion();
         }
